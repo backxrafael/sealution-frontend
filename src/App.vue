3cddc2ffac629cc67ac7e40048dea95af7e02bcd
Rafael Backx <rafael@sentigrate.com>
2024-06-17 09:56:30 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Sealution frontend</h1>
+  <input type="text" v-model="databaseName" placeholder="create database">
+  <button @click="onCreateDBClick"> Create database </button>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+<script setup>
+import { ref } from 'vue';
+const databaseName = ref("");
+async function onCreateDBClick() {
+  const name = databaseName.value;
+  if (name.includes(' ')) {
+    console.log('name cannot contain spaces');
+    return;
   }
+  console.log(name);
+  const result = await fetch(`${process.env.VUE_APP_BACKEND_URL}/v1/database/${name}`);
+  console.log(result);
 }
 </script>
 
